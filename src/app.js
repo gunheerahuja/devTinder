e324d03190e39a3cba2eeb84ee066e5173a47762
gunheer <gunheerahuja@gmail.com>
2024-11-12 02:14:59 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 
 const app = express();
-
+//the sequence of routes that will be executed when the application is started with express app 
 
 app.use((req,res) => {
     res.send('Welcome');
+});
+
+app.use("/hello",(req, res) => {
+    res.send('Hello World');
+});
+
+
+app.use("/",(req, res) => {
+    res.send('About Us');
 });
 app.listen(2777,()=>{
     console.log('Server started on port 2777');
